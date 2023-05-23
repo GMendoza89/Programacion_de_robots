681f24b594000b84220488e93c05bb9b5c8f2951
Gustavo David Mendoza Pinto <gustavo.mendoza.dp@gmail.com>
2023-05-23 13:30:57 -0600

    Se inician cambios para ciclo 23-3

--- Python_Scrips/Hola_mundo.py
+++
@@ -1,9 +0,0 @@
-mensaje = "Hola mundo\n Esta materia es \n Programacion de robots"
-print(mensaje)
-edad = 18
-print(edad)
-Pi = 3.1416
-print(Pi)
-print(type(mensaje))
-print(type(edad))
-print(type(Pi))
